14e0c08e4d7c1ee960e34efa80b49b039d08ddb9
Aditya Gupta <guptaaditya008@gmail.com>
2023-03-17 11:06:48 +0000

    modified models and added referential tests

--- dbt/models/raw/stg__checkins.sql
+++ dbt/models/raw/stg__checkins.sql
@@ -2,6 +2,11 @@ with
     stg__checkins_cte as (
         select business_id, date from {{ source("yelp", "stg_checkin") }}
     )
-
-select *
+select
+    business_id,
+    if(
+        date is null or date = '',
+        null,
+        array(select timestamp(trim(d)) from unnest(split(date, ',')) as d)
+    ) as checkin_dates
 from stg__checkins_cte
