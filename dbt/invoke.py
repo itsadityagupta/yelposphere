6d5e687d8d6d17764dd8c9954c25e71b2540fe26
Aditya-Gupta1 <guptaaditya008@gmail.com>
2023-04-03 18:30:32 +0530

    trying python again

--- dbt/invoke.py
+++ dbt/invoke.py
@@ -1,32 +1,18 @@
 import subprocess
 import logging
 import os
-import http.server
-import socketserver
+
+from flask import Flask
+
+app = Flask(__name__)
 
 
-class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        logging.info("received a request")
-        try:
-            subprocess.check_call(["/bin/sh", "script.sh"])
-        except subprocess.CalledProcessError as e:
-            logging.error("error running script: %s", e)
+@app.route("/")
+def run_dbt():
+    logging.info("Received a request. Running DBT commands...")
+    result_code = subprocess.call(["/bin/sh", "script.sh"], timeout=300)
+    return result_code
 
 
-logging.basicConfig(level=logging.INFO)
-
-PORT = int(os.environ.get("PORT", 8080))
-logging.info("starting server on port %s", PORT)
-
-Handler = MyHttpRequestHandler
-httpd = socketserver.TCPServer(("", PORT), Handler)
-logging.info("serving at port %s", PORT)
-
-try:
-    httpd.serve_forever()
-except KeyboardInterrupt:
-    pass
-
-httpd.server_close()
-logging.info("server stopped")
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
