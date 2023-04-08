632e2587d60084af183f30c9ad151811358e1e53
Aditya-Gupta1 <guptaaditya008@gmail.com>
2023-04-08 12:50:48 +0530

    create architecture diagram

---
+++ utils/architecture_diagram.py
@@ -0,0 +1,34 @@
+from diagrams import Diagram, Cluster
+from diagrams.gcp.analytics import Dataproc
+from diagrams.gcp.storage import GCS
+from diagrams.gcp.compute import Run
+from diagrams.custom import Custom
+from diagrams.gcp.devtools import Build, ContainerRegistry
+from diagrams.gcp.analytics import BigQuery, Composer
+
+graph_attr = {
+    "fontsize": "20"
+}
+
+cluster_attr = {
+    "fontsize": "16"
+}
+
+with Diagram("Yelposphere Architecture Diagram", graph_attr=graph_attr):
+    with Cluster("Cloud Composer Environment", graph_attr=cluster_attr):
+        with Cluster(""):
+            gcs = GCS("GCS")
+            dataproc = Dataproc("Dataproc")
+            bigquery = BigQuery("BigQuery")
+            run = Run("Cloud Run")
+
+        composer = Composer("Cloud Composer")
+
+    build = Build("Cloud Build")
+    registry = ContainerRegistry("Artifact Registry")
+    repo = Custom("Github Repo", "repo.png")
+
+    gcs >> dataproc >> bigquery << run
+    run << registry
+    registry << build
+    build << repo
